4b2c4f407a446241e511a3d0eb618bcb4f284e0f
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-04-05 16:57:56 -0700

    Updated temp.py to handle differently shaped data

--- c++_dnest/temp.py
+++ c++_dnest/temp.py
@@ -4,16 +4,18 @@ import time
 sample = atleast_2d(loadtxt('sample.txt'))
 data = loadtxt('data_100stars.txt')
 sample = sample[:,5:]
+
 ion()
 hold(False)
 for i in xrange(0, sample.shape[0]):
+	mock = sample[i,:].reshape(100, 100)
 	subplot(1,3,1)
-	imshow(data.reshape(100,100), interpolation='none')
+	imshow(data, interpolation='none')
 	subplot(1,3,2)
-	imshow(sample[i,:].reshape(100,100), interpolation='none')
+	imshow(mock, interpolation='none')
 	title(str(i+1))
 	subplot(1,3,3)
-	imshow((sample[i,:] - data).reshape(100,100), interpolation='none')
+	imshow(mock - data, interpolation='none')
 	draw()
 	print([i, max(sample[i,:])])
 ioff()

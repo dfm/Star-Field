ecdcbaa096e5620e9f88528c4d8f963b681a8fd8
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-04-19 17:27:11 -0700

    Simplified Data class

--- c++_dnest/Data.cpp
+++ c++_dnest/Data.cpp
@@ -1,28 +1,12 @@
 #include "Data.h"
 #include <fstream>
-#include <iostream>
-#include <cmath>
 
 using namespace std;
 
-const int Data::ni = 100;
-const int Data::nj = 100;
-const double Data::xMin = -1.0;
-const double Data::xMax =  1.0;
-const double Data::yMin = -1.0;
-const double Data::yMax =  1.0;
-const double Data::xRange = (Data::xMax - Data::xMin);
-const double Data::yRange = (Data::yMax - Data::yMin);
-const double Data::range = sqrt(Data::xRange*Data::yRange);
-const double Data::dx = Data::xRange/(Data::nj - 1);
-const double Data::dy = Data::yRange/(Data::ni - 1);
-const vector<Array> Data::xy = Array::meshgrid(Data::ni, Data::nj, Data::xMin, Data::xMax, Data::yMin, Data::yMax);
-const Array& Data::x = Data::xy[0];
-const Array& Data::y = Data::xy[1];
+const int Data::numPixels = 100;
 
 Data::Data()
-:pixels(ni, nj)
-,loaded(false)
+:pixels(numPixels, numPixels)
 {
 
 }
@@ -30,26 +14,9 @@ Data::Data()
 void Data::load(const char* filename)
 {
 	fstream fin(filename, ios::in);
-	for(int i=0; i<ni; i++)
-		for(int j=0; j<nj; j++)
-			fin>>pixels[i][j];
+	for(int i=0; i<numPixels; i++)
+		for(int j=0; j<numPixels; j++)
+			fin>>pixels(i, j);
 	fin.close();
-	loaded = true;
-}
-
-bool Data::isLoaded() const
-{
-	return loaded;
-}
-
-
-vector<double>& Data::operator [] (int i)
-{
-	return pixels[i];
-}
-
-const vector<double>& Data::operator [] (int i) const
-{
-	return pixels[i];
 }
 
--- c++_dnest/Data.h
+++ c++_dnest/Data.h
@@ -7,29 +7,18 @@
 
 class Data
 {
-	friend class Star;
-
-	public:
-		// Hard coded image size and bounds for now
-		static const int ni, nj;
-		static const double xMin, xMax, yMin, yMax;
-		static const double xRange, yRange, range;
-		static const double dx, dy; // Pixel widths
-		static const std::vector<Array> xy;	// Coordinates of pixel centers
-				         		// These are basically meshgrids
-		static const Array& x; // Just references to the elements of xy
-		static const Array& y;
-
 	private:
-		Array pixels; // Pixel intensities 
-		bool loaded;
+		// Hard coded for now
+		static const int numPixels;
+
+		Array pixels; // Pixel intensities
 
 	public:
 		Data();
 		void load(const char* filename);
-		bool isLoaded() const;
-		const std::vector<double>& operator [] (int i) const; // To access values
-		std::vector<double>& operator [] (int i); //Non-const version
+
+		// Read-only access to values
+		double operator () (int i, int j) const;
 };
 
 #endif

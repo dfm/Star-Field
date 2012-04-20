20fe90de913a25b296d7813d9b66d86bb22e7448
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-04-19 17:13:23 -0700

    Redefined Array as just an Eigen Array of doubles

--- c++_dnest/Array.h
+++ c++_dnest/Array.h
@@ -1,32 +1,9 @@
 #ifndef _Array_
 #define _Array_
 
-#include <vector>
-#include <ostream>
+#include <Eigen/Core>
 
-class Array
-{
-	private:
-		int ni, nj;
-		std::vector< std::vector<double> > values;
-
-	public:
-		Array(int ni, int nj);
-		Array(int ni, int nj, double value);
-
-		void zero();
-		double sum() const;
-		void normalise();
-		int getNi() const;
-		int getNj() const;
-
-		const std::vector<double>& operator [] (int i) const; // For element access
-		std::vector<double>& operator [] (int i); // Non-const case
-		Array& operator += (const Array& other);
-
-		static std::vector<Array> meshgrid(int ni, int nj, double xMin, double xMax, double yMin, double yMax);
-		friend std::ostream& operator << (std::ostream& out, const Array& array);
-};
+typedef Eigen::Array<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> Array;
 
 #endif
 

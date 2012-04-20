a031e4f1838416201e42f3294be5d6ccb1ebe764
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-04-19 17:14:46 -0700

    Star is now very basic

--- c++_dnest/Star.cpp
+++ c++_dnest/Star.cpp
@@ -14,26 +14,3 @@ Star::Star(double x, double y, double flux)
 
 }
 
-Array& Star::incrementImage(Array& image, const PSF& psf, double coefficient) const
-{
-	// Calculate the image of just this star on the data grid
-	Array starImage(Data::ni, Data::nj, 0.0);
-	for(int i=0; i<Data::ni; i++)
-		for(int j=0; j<Data::nj; j++)
-			starImage[i][j] += flux*coefficient*(psf.evaluate(Data::x[i][j] - x, Data::y[i][j] - y));
-
-	// Increment image
-	image += starImage;
-	return image;
-}
-
-Array& Star::incrementImage(Array& image, const PSF& psf) const
-{
-	return incrementImage(image, psf, 1.0);
-}
-
-Array& Star::decrementImage(Array& image, const PSF& psf) const
-{
-	return incrementImage(image, psf, -1.0);
-}
-

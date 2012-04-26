99ad02aeb91bef0d2f029884eae8ab647e2f324a
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-04-26 13:43:41 -0700

    Data class now has pixel centers in it

--- c++_dnest/Data.cpp
+++ c++_dnest/Data.cpp
@@ -12,11 +12,33 @@ Data::Data()
 
 void Data::load(const char* filename)
 {
+	// Load the data
 	fstream fin(filename, ios::in);
 	if(!fin)
 		cerr<<"Error: Can't load "<<filename<<"."<<endl;
 	fin>>image;
 	fin.close();
 	loaded = true;
+
+
+	vector<double> x(image.get_nJ());
+	vector<double> y(image.get_nI());
+	for(int j=0; j<image.get_nJ(); j++)
+		x[j] = image.get_xMin() + (j + 0.5)*image.get_dx();
+	for(int i=0; i<image.get_nI(); i++)
+		y[i] = image.get_yMax() - (i + 0.5)*image.get_dy();
+
+	// Assign the xc, yc arrays
+	xc = image.get_pixels();
+	yc = image.get_pixels();
+	for(int i=0; i<image.get_nI(); i++)
+	{
+		for(int j=0; j<image.get_nJ(); j++)
+		{
+			xc(i, j) = x[j];
+			yc(i, j) = y[i];
+		}
+	}
+
 }
 
--- c++_dnest/Data.h
+++ c++_dnest/Data.h
@@ -1,7 +1,9 @@
 #ifndef _Data_
 #define _Data_
 
+#include <vector>
 #include "Image.h"
+#include "Array.h"
 
 /*
 * An instance of this class represents the data set.
@@ -14,12 +16,19 @@ class Data
 		Image image; // Pixel intensities
 		bool loaded;
 
+		// Pixel centers
+		Array xc, yc;
+
 	public:
 		Data();
 		void load(const char* filename);
 
 		// Read-only getter for the image
-		const Image& get_image() { return image; }
+		const Image& get_image() const { return image; }
+
+		// Getters for xc and yc
+		double get_xc(int i, int j) const { return xc(i, j); }
+		double get_yc(int i, int j) const { return yc(i, j); }
 };
 
 #endif

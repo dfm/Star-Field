c14571e9892f5d0cd158f06dfe5d7ac697f1dd93
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-04-26 18:19:16 -0700

    Reinstated image incrementation methods in Star

--- c++_dnest/Star.cpp
+++ c++_dnest/Star.cpp
@@ -14,3 +14,34 @@ Star::Star(double x, double y, double flux)
 
 }
 
+Array& Star::incrementImage(Array& image, const PSF& psf, const Data& data, double coefficient) const
+{
+	// Calculate the image of just this star on the data grid
+	Array starImage(data.get_ni(), data.get_nj());
+	starImage.setZero();
+
+	for(int i=0; i<data.get_ni(); i++)
+	{
+		for(int j=0; j<data.get_nj(); j++)
+		{
+			starImage(i, j) += flux*coefficient*
+	(psf.evaluate(data.get_xc(i, j) - x, data.get_yc(i, j) - y));
+		}
+	}
+
+	// Increment image
+	image += starImage;
+
+	return image;
+}
+
+Array& Star::incrementImage(Array& image, const PSF& psf, const Data& data) const
+{
+	return incrementImage(image, psf, data, 1.0);
+}
+
+Array& Star::decrementImage(Array& image, const PSF& psf, const Data& data) const
+{
+	return incrementImage(image, psf, data, -1.0);
+}
+
--- c++_dnest/Star.h
+++ c++_dnest/Star.h
@@ -1,6 +1,10 @@
 #ifndef _Star_
 #define _Star_
 
+#include "Array.h"
+#include "PSF.h"
+#include "Data.h"
+
 class Star
 {
 	public:
@@ -9,6 +13,14 @@ class Star
 	public:
 		Star();
 		Star(double x, double y, double flux);
+
+		// Make an image of the star on the same shape and bounds as the given data
+		// and add it to the input image. Returns the input image again by reference just for niceness
+		Array& incrementImage(Array& image, const PSF& psf, const Data& data) const; // Default to coefficient=1
+		Array& decrementImage(Array& image, const PSF& psf, const Data& data) const; // Default to coefficient=-1
+
+		// Allows for subtraction, scaling, etc via the coefficient
+		Array& incrementImage(Array& image, const PSF& psf, const Data& data, double coefficient) const; 
 };
 
 #endif

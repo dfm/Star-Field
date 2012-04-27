ba8603619156fce8abb84464e4708ba6b66a9ee3
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-04-26 18:06:57 -0700

    Wrote some getters

--- c++_dnest/Data.h
+++ c++_dnest/Data.h
@@ -24,6 +24,18 @@ class Data
 		Data();
 		void load(const char* filename);
 
+		// Various getters
+		int get_ni() const { return ni; }
+		int get_nj() const { return nj; }
+		double get_xMin() const { return xMin; }
+		double get_xMax() const { return xMax; }
+		double get_yMin() const { return yMin; }
+		double get_yMax() const { return yMax; }
+		double get_xRange() const { return xRange; }
+		double get_yRange() const { return yRange; }
+		double get_dx() const { return dx; }
+		double get_dy() const { return dy; }
+
 		// Getters for xc and yc
 		double get_xc(int i, int j) const { return xc(i, j); }
 		double get_yc(int i, int j) const { return yc(i, j); }

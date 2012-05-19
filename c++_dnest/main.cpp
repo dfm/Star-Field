8d4cde3b855d96f36c1f1969f1d93888d8abb2af
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-19 16:18:18 -0700

    DNest3-style main

--- c++_dnest/main.cpp
+++ c++_dnest/main.cpp
@@ -1,121 +1,33 @@
 /*
-    Copyright (C) 2011 Brendon J. Brewer
-    This file is part of DNest, the Diffusive Nested Sampler.
-
-    DNest is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    DNest is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DNest.  If not, see <http://www.gnu.org/licenses/>.
+* Copyright (c) 2009, 2010, 2011, 2012 Brendon J. Brewer.
+*
+* This file is part of DNest.
+*
+* DNest is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* DNest is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with DNest. If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #include <iostream>
+#include "Start.h"
+#include "Data.h"
 #include "StarFieldModel.h"
-#include "Hyperparameters/Pareto.h"
-#include "MultiSampler.h"
-#include "RandomNumberGenerator.h"
-#include "DNestSampler.h"
-#include <boost/thread.hpp>
-#include <boost/program_options.hpp>
 
 using namespace std;
-using namespace DNest;
-namespace po = boost::program_options;
-
-void runMultiThread(const Model* exampleModel, const string& levelsFile);				// Run multithreaded sampler
-void runLegacy(const Model* exampleModel, const string& levelsFile);	// Run legacy sampler
 
 int main(int argc, char** argv)
 {
-	Data::load_data("SimulatedData/sparse.txt");
-
-	// What kind of model will we be running?
-	StarFieldModel<Pareto> exampleModel;
-
-	// Which kind of sampler, and should we pre-load level structure?
-	bool legacy = false;
-	string levelsFile = "";
-
-	// Handle command line options
-	{
-		// Declare the supported options.
-		po::options_description desc("Welcome to DNest. Allowed options");
-		desc.add_options()
-		    ("help", "produce help message")
-		    ("levels", po::value<string>(), "Input levels file")
-		    ("legacy", "use legacy sampler (serial)")
-		;
-
-		po::variables_map vm;
-		po::store(po::parse_command_line(argc, argv, desc), vm);
-		po::notify(vm);    
-
-		if (vm.count("help"))
-		{
-		    cout<<desc<<endl;
-		    return 1;
-		}
-
-		if(vm.count("legacy")) 
-			legacy = true;
-
-		if(vm.count("levels"))
-			levelsFile = vm["levels"].as<string>();
-	}
-
-	// Run the sampler!
-	if(legacy)
-		runLegacy(&exampleModel, levelsFile);
-	else
-		runMultiThread(&exampleModel, levelsFile);		
-
+	Data::get_data().load("SimulatedData/sparse.txt");
+	DNest3::start<StarFieldModel>(argc, argv);
 	return 0;
-}
-
-void runLegacy(const Model* exampleModel, const string& levelsFile)
-{
-	RandomNumberGenerator::initialise(-time(0));
-
-	if(levelsFile == "")
-	{
-		DNestSampler sampler(exampleModel);
-		sampler.run();
-	}
-	else
-	{
-		DNestSampler sampler(exampleModel, levelsFile.c_str());
-		sampler.run();
-	}
-
-	RandomNumberGenerator::free();
-
-}
-
-void runMultiThread(const Model* exampleModel, const string& levelsFile)
-{
-	// Multithreaded
-	int threads = boost::thread::hardware_concurrency();
-	cout<<"# Running on "<<threads<<" threads."<<endl;
-
-	DNestOptions options("OPTIONS");
-
-	if(levelsFile == "")
-	{
-		MultiSampler sampler(threads, exampleModel, options);
-		sampler.runThreaded();
-	}
-	else
-	{
-		MultiSampler sampler(threads, exampleModel, options, levelsFile.c_str());
-		sampler.runThreaded();
-	}
 }
 

17aac7be509bd3b43790ce5c999ad337b8902366
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-05-22 00:22:57 -0700

    Uniform in new framework

--- c++_dnest/Hyperparameters/Uniform.cpp
+++ c++_dnest/Hyperparameters/Uniform.cpp
@@ -2,70 +2,65 @@
 #include <Utils.h>
 #include <RandomNumberGenerator.h>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 using namespace DNest3;
 
-const double Uniform::logMinOnFraction = log(1E-3);
-const double Uniform::logMaxOnFraction = log(1.);
-const double Uniform::logMinMu = log(1E-3);
-const double Uniform::logMaxMu = log(1E3);
+Uniform::Uniform()
+:minLogMu(log(1E-3))
+,maxLogMu(log(1E3))
+,rangeLogMu(maxLogMu - minLogMu)
+{
+
+}
 
 void Uniform::fromPrior()
 {
-	onFraction = exp(logMinOnFraction +
-			(logMaxOnFraction - logMinOnFraction)*randomU());
-	mu = exp(logMinMu + (logMaxMu - logMinMu)*randomU());
+	mu = exp(minLogMu + rangeLogMu*randn());
 }
 
-double Uniform::perturb()
+double Uniform::perturb1(vector<Star>& stars)
 {
-	int which = randInt(2);
+	double logH = 0.;
+	double proposal = log(mu);
+	proposal += rangeLogMu*pow(10., 1.5 - 6.*randomU())*randn();
+	proposal = mod(proposal - minLogMu, rangeLogMu) + minLogMu;
+	proposal = exp(proposal);
 
-	if(which == 0)
-	{
-		mu = log(mu);
-		mu += (logMaxMu - logMinMu)
-			*pow(10., 1.5 - 6.*randomU())*randn();
-		mu = mod(mu - logMinMu, logMaxMu - logMinMu) + logMinMu;
-		mu = exp(mu);
-	}
-	else
-	{
-		onFraction = log(onFraction);
-		onFraction += (logMaxOnFraction - logMinOnFraction)
-				*pow(10., 1.5 - 6*randomU())*randn();
-		onFraction = mod(onFraction - logMinOnFraction,
-			logMaxOnFraction - logMinOnFraction) + logMinOnFraction;
-		onFraction = exp(onFraction);
-	}
+	double ratio = proposal/mu;
+	for(size_t i=0; i<stars.size(); i++)
+		stars[i].flux *= ratio;
+	mu = proposal;
 
-	return 0.;
+	return logH;
 }
 
-void Uniform::transform(double u_x, double u_y, double u_f,
-				double& x, double& y, double& f) const
+double Uniform::perturb2(const vector<Star>& stars)
 {
-	double t = 1. - onFraction;
-	// Compute flux
-	if(u_f < t)
-		f = 0.;
-	else
-	{
-		double U = (u_f - t)/onFraction; // U(0, 1)
-		f = -mu*log(1. - U); // Must use CDF not 1-CDF
-	}
+	double logH = 0.;
+	double proposal = log(mu);
+	proposal += rangeLogMu*pow(10., 1.5 - 6.*randomU())*randn();
+	proposal = mod(proposal - minLogMu, rangeLogMu) + minLogMu;
+	proposal = exp(proposal);
 
-	// Compute position
-	x = (Data::get_data().get_xMin() - 0.1*Data::get_data().get_xRange())
-		 + 1.2*Data::get_data().get_xRange()*u_x;
-	y = (Data::get_data().get_yMin() - 0.1*Data::get_data().get_yRange())
-		 + 1.2*Data::get_data().get_yRange()*u_y;
+	double logMu1 = log(mu);
+	double logMu2 = log(proposal);
+	for(size_t i=0; i<stars.size(); i++)
+	{
+		if(stars[i].flux < 0.)
+			cerr<<"# Warning: Negative flux star."<<endl;
+		logH -= -logMu1 - stars[i].flux/mu;
+		logH += -logMu2 - stars[i].flux/proposal;
+	}
+	mu = proposal;
+	return logH;
 }
+
 
 void Uniform::print(ostream& out) const
 {
-	out<<onFraction<<' '<<mu;
+	out<<mu;
 }
 
 
--- c++_dnest/Hyperparameters/Uniform.h
+++ c++_dnest/Hyperparameters/Uniform.h
@@ -10,28 +10,28 @@
 class Uniform:public Hyperparameters
 {
 	private:
-		static const double logMinOnFraction, logMaxOnFraction;
-		static const double logMinMu, logMaxMu;
+		const double minLogMu, maxLogMu, rangeLogMu;
 
-		double onFraction; // Fraction of stars that are on
 		double mu; // Mean flux of stars
 
 	public:
+		Uniform();
+
 		// Generate hyperparameters from the prior
 		void fromPrior();
 
-		// Metropolis-Hastings proposal
-		double perturb();
+		// Metropolis-Hastings proposal - move stars
+		double perturb1(std::vector<Star>& stars);
 
-		/*
-		* Transform U(0, 1) latent variables into positions 
-		* and fluxes
-		*/
-		void transform(double u_x, double u_y, double u_f,
-				double& x, double& y, double& f) const;
+		// Metropolis-Hastings proposal - don't move stars
+		double perturb2(const std::vector<Star>& stars);
 
 		/* Print the hyperparameters */
 		void print(std::ostream& out) const;
+
+		// Generate a star
+		Star generateStar() const;
+
 };
 
 #endif

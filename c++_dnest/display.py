168a6131da48d4726d31126933994d1ce126e0a6
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-06-15 13:27:08 -0700

    Show posterior mean images

--- c++_dnest/display.py
+++ c++_dnest/display.py
@@ -1,7 +1,7 @@
 from pylab import *
 import time, os
 
-saveFrames = False # For making movies
+saveFrames = True # For making movies
 if saveFrames:
 	os.system('rm Frames/*.png')
 
@@ -22,6 +22,8 @@ images =   sample[:, numHyperparams+3*maxNumStars:]
 xCatalog = np.array([])
 yCatalog = np.array([])
 fCatalog = np.array([])
+
+mock_data_mean = np.zeros(data.shape)
 
 ion()
 hold(False)
@@ -53,10 +55,13 @@ for i in xrange(0, sample.shape[0]):
 	title('Residuals')
 	draw()
 
-	print(i+1, ((((data - mock)/30.)**2).sum()/10000. - 1.)/(sqrt(2.)/100.))
+	mock_data_mean += mock/sample.shape[0]
+
 	xCatalog = hstack([xCatalog, xStars[i,which]])
 	yCatalog = hstack([yCatalog, yStars[i,which]])
 	fCatalog = hstack([fCatalog, fStars[i,which]])
+
+	print(i+1, ((((data - mock)/30.)**2).sum()/10000. - 1.)/(sqrt(2.)/100.))
 
 	if saveFrames:
 		savefig('Frames/' + '%0.4d'%(i+1) + '.png', bbox_inches='tight')
@@ -64,6 +69,35 @@ for i in xrange(0, sample.shape[0]):
 ioff()
 show()
 
-img = histogram2d(xCatalog, yCatalog, bins=100, weights=fCatalog)[0].T[::-1]
-imshow(img)
+subplot(1,3,1)
+imshow(data, extent=(-1, 1, -1, 1), cmap='gray')
+title('Data')
+axis([-1, 1, -1, 1])
+gca().set_xticks([-1, 0, 1])
+gca().set_yticks([-1, 0, 1])
+xlabel('$x$')
+ylabel('$y$')
+
+subplot(1,3,2)
+imshow(mock_data_mean, extent=(-1, 1, -1, 1), cmap='gray')
+title('Denoised')
+axis([-1, 1, -1, 1])
+gca().set_xticks([-1, 0, 1])
+gca().set_yticks([-1, 0, 1])
+xlabel('$x$')
+ylabel('$y$')
+
+
+subplot(1,3,3)
+img = histogram2d(xCatalog, yCatalog, bins=200, weights=fCatalog)[0].T[::-1]
+img = img/img.max()
+imshow(img**0.25, extent=(-1.1, 1.1, -1.1, 1.1), cmap='gray')
+title('Denoised and Deblurred')
+axis([-1, 1, -1, 1])
+gca().set_xticks([-1, 0, 1])
+gca().set_yticks([-1, 0, 1])
+xlabel('$x$')
+ylabel('$y$')
+
 show()
+

a7f3c3ea9c620f032715d48c69d784c63f130e76
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-07-26 16:39:30 +1200

    BrokenPareto -- just cloned Pareto

---
+++ c++_dnest/Hyperparameters/BrokenPareto.cpp
@@ -0,0 +1,137 @@
+#include "BrokenPareto.h"
+#include <Utils.h>
+#include <RandomNumberGenerator.h>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+using namespace DNest3;
+
+BrokenPareto::BrokenPareto()
+:minLogMu(log(1E-3))
+,maxLogMu(log(1E3))
+,rangeLogMu(maxLogMu - minLogMu)
+{
+	if(!Data::get_data().isLoaded())
+		cerr<<"# Error. No data has been loaded."<<endl;
+
+	xMin = Data::get_data().get_xMin() - 0.1*Data::get_data().get_xRange();
+	xMax = Data::get_data().get_xMax() + 0.1*Data::get_data().get_xRange();
+	yMin = Data::get_data().get_yMin() - 0.1*Data::get_data().get_yRange();
+	yMax = Data::get_data().get_yMax() + 0.1*Data::get_data().get_yRange();
+}
+
+void BrokenPareto::fromPrior()
+{
+	mu = exp(minLogMu + rangeLogMu*randn());
+	alpha = 1. + 4.*randomU();
+}
+
+double BrokenPareto::perturbMu()
+{
+	double logH = 0.;
+	mu = log(mu);
+	mu += rangeLogMu*pow(10., 1.5 - 6.*randomU())*randn();
+	mu = mod(mu - minLogMu, rangeLogMu) + minLogMu;
+	mu = exp(mu);
+	return logH;
+}
+
+double BrokenPareto::perturbAlpha()
+{
+	double logH = 0.;
+	alpha += 4.*pow(10., 1.5 - 6.*randomU())*randn();
+	alpha = mod(alpha - 1., 4.) + 1.;
+	return logH;
+}
+
+double BrokenPareto::perturb1(vector<Star>& stars)
+{
+	double logH = 0.;
+
+	for(size_t i=0; i<stars.size(); i++)
+		stars[i].flux = fluxCDF(stars[i].flux);
+
+	int which = randInt(2);
+	if(which == 0)
+		logH += perturbMu();
+	else if(which == 1)
+		logH += perturbAlpha();
+
+	for(size_t i=0; i<stars.size(); i++)
+		stars[i].flux = fluxInvCDF(stars[i].flux);
+	
+	return logH;
+}
+
+double BrokenPareto::perturb2(const vector<Star>& stars)
+{
+	double logH = 0.;
+
+	for(size_t i=0; i<stars.size(); i++)
+		logH -= fluxLogPDF(stars[i].flux);
+
+	int which = randInt(2);
+	if(which == 0)
+		logH += perturbMu();
+	else if(which == 1)
+		logH += perturbAlpha();
+
+	for(size_t i=0; i<stars.size(); i++)
+		logH += fluxLogPDF(stars[i].flux);
+	
+	return logH;
+}
+
+void BrokenPareto::print(ostream& out) const
+{
+	out<<mu<<' '<<alpha;
+}
+
+Star BrokenPareto::generateStar() const
+{
+	double x = xMin + (xMax - xMin)*randomU();
+	double y = yMin + (yMax - yMin)*randomU();
+	double f = fluxInvCDF(randomU());
+	return Star(x, y, f);
+}
+
+double BrokenPareto::perturbStar1(Star& star, double scale) const
+{
+	double logH = 0.;
+	star.x += (xMax - xMin)*scale*randn();
+	star.y += (yMax - yMin)*scale*randn();
+	star.x = mod(star.x - xMin, xMax - xMin) + xMin;
+	star.y = mod(star.y - yMin, yMax - yMin) + yMin;
+	return logH;
+}
+
+double BrokenPareto::perturbStar2(Star& star, double scale) const
+{
+	double logH = 0.;
+
+	double u = fluxCDF(star.flux);
+	u += scale*randn();
+	u = mod(u, 1.);
+	star.flux = fluxInvCDF(u);
+
+	return logH;
+}
+
+double BrokenPareto::fluxCDF(double f) const
+{
+	return 1. - pow(mu/f, alpha);
+}
+
+double BrokenPareto::fluxInvCDF(double u) const
+{
+	return mu*pow(1. - u, -1./alpha);
+}
+
+double BrokenPareto::fluxLogPDF(double f) const
+{
+	if(f < mu)
+		return -1E300;
+	return log(alpha) + alpha*log(mu) - (alpha + 1.)*log(f);
+}
+
---
+++ c++_dnest/Hyperparameters/BrokenPareto.h
@@ -0,0 +1,54 @@
+#ifndef _BrokenPareto_
+#define _BrokenPareto_
+
+#include "../Hyperparameters.h"
+#include <ostream>
+
+/*
+* Stars can be anywhere, but their fluxes are tied together
+*/
+class BrokenPareto:public Hyperparameters
+{
+	private:
+		double minLogMu, maxLogMu, rangeLogMu;
+		double xMin, xMax, yMin, yMax;
+
+		double mu; // Minimum flux of stars
+		double alpha; // Slope
+
+		// These are useful for perturbStar1 and 2
+		double perturbMu();
+		double perturbAlpha();
+
+		// These are useful for perturbStar2
+		double fluxCDF(double f) const;
+		double fluxInvCDF(double u) const;
+		double fluxLogPDF(double f) const;
+
+	public:
+		BrokenPareto();
+
+		// Generate hyperparameters from the prior
+		void fromPrior();
+
+		// Metropolis-Hastings proposal - move stars
+		double perturb1(std::vector<Star>& stars);
+
+		// Metropolis-Hastings proposal - don't move stars
+		double perturb2(const std::vector<Star>& stars);
+
+		/* Print the hyperparameters */
+		void print(std::ostream& out) const;
+
+		// Generate a star
+		Star generateStar() const;
+
+		// Perturb a star (position)
+		double perturbStar1(Star& star, double scale) const;
+
+		// Perturb a star (flux)
+		double perturbStar2(Star& star, double scale) const;
+};
+
+#endif
+

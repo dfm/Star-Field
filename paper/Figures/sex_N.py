22d768e00681e24d8e24f7d06090538c69bc99da
Dan F-M <danfm@nyu.edu>
2012-11-12 08:37:28 -0500

    adding sextractor results

--- paper/Figures/sex_N.py
+++ paper/Figures/sex_N.py
@@ -1,5 +1,6 @@
+import os
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
 
 # Directories
 directories = ['break2', 'crowded']
@@ -8,30 +9,28 @@ directories = ['break2', 'crowded']
 sigmas = ['0.5', '1.5', '2.5', '3.5', '4.5', '5.5', '6.5']
 files = ['stars.' + sigma + '.cat' for sigma in sigmas]
 
-plt.figure(figsize=(16, 6))
-loc=[3, 5]
+pl.figure(figsize=(16, 6))
+loc = [3, 5]
 
 for i in xrange(0, 2):
-	# Load catalogs
-	catalogs = []
-	for f in files:
-		catalogs.append(np.loadtxt(directories[i] + '/' + f))
+    # Load catalogs
+    catalogs = []
+    for f in files:
+        catalogs.append(np.loadtxt(os.path.join(directories[i], f)))
 
-	# Convert sigmas to floats
-	sigma = np.array([float(sig) for sig in sigmas])
+    # Convert sigmas to floats
+    sigma = np.array([float(sig) for sig in sigmas])
 
-	# Number of stars in each catalog
-	N = np.array([cat.shape[0] for cat in catalogs])
+    # Number of stars in each catalog
+    N = np.array([cat.shape[0] for cat in catalogs])
 
-	plt.subplot(1,2,i+1)
-	plt.plot(sigma, N, 'bo-', label='SExtractor Inference')
-	plt.axhline(82*10**i, color='k', linestyle='--', linewidth=2, label='True Value')
-	plt.xlabel('Cutoff (number of $\\sigma$s)', fontsize=15)
-	plt.ylabel('Number of Stars $N$', fontsize=15)
-	plt.ylim(0)
-	plt.legend(loc=loc[i])
-	plt.title('Test Case ' + str(i+1), fontsize=15)
+    pl.subplot(1, 2, i + 1)
+    pl.plot(sigma, N, 'o-', color="#333333", ec="k")
+    pl.axhline(82 * 10 ** i, color='k', linestyle='--', linewidth=2)
+    pl.xlabel('Cutoff (number of $\\sigma$s)')
+    pl.ylabel('Number of Stars $N$')
+    pl.ylim(0)
+    pl.legend(loc=loc[i])
+    pl.title('Test Case ' + str(i + 1))
 
-plt.#savefig('sex_N.eps', bbox_inches='tight')
-plt.show()
-
+pl.savefig('sex_N.eps', bbox_inches='tight')

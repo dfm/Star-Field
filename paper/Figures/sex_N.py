9740ae766d4440b0d92f6ff0c779e7487b4a7f5e
Dan F-M <danfm@nyu.edu>
2012-11-12 08:46:48 -0500

    Updating plots of SExtractor results

--- paper/Figures/sex_N.py
+++ paper/Figures/sex_N.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as pl
+from matplotlib.ticker import MaxNLocator
 
 # Directories
 directories = ['break2', 'crowded']
@@ -9,9 +10,10 @@ directories = ['break2', 'crowded']
 sigmas = ['0.5', '1.5', '2.5', '3.5', '4.5', '5.5', '6.5']
 files = ['stars.' + sigma + '.cat' for sigma in sigmas]
 
-pl.figure(figsize=(10, 4))
+pl.figure(figsize=(11, 4))
 loc = [3, 5]
 
+pl.subplots_adjust(wspace=0.3)
 for i in xrange(0, 2):
     # Load catalogs
     catalogs = []
@@ -25,11 +27,13 @@ for i in xrange(0, 2):
     N = np.array([cat.shape[0] for cat in catalogs])
 
     pl.subplot(1, 2, i + 1)
-    pl.plot(sigma, N, 'ko-', mfc="#888888")
+    pl.plot(sigma, N, 'ko-', mfc="#888888", mew=1.5, lw=2)
     pl.axhline(82 * 10 ** i, color='k', linestyle='--', linewidth=2)
     pl.xlabel(r'Detection Threshold ($\sigma$)')
     pl.ylabel('Number of Stars $N$')
     pl.ylim(0)
+    pl.gca().xaxis.set_major_locator(MaxNLocator(4))
+    pl.gca().yaxis.set_major_locator(MaxNLocator(4))
     pl.title('Test Case ' + str(i + 1))
 
 pl.savefig('sex_N.eps', bbox_inches='tight')

f06778e2e7a46c5bc638c9ac35fa159ae8d200c1
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-11-20 10:35:12 +1300

    Initial kinda-working JAGS model

---
+++ JAGS/main.r
@@ -0,0 +1,21 @@
+library(rjags)
+
+# create the data to be sent to R (using the same as class example):
+#data <- source('data.txt')$value
+
+# now to send the model and data to JAGS:
+# if you need to navigate to the place you saved your .bug file, now is the time to do it.
+m = jags.model(file = "model.txt")#, data=data)
+
+# this will create a BUGS object in R. Now you can do some runs...
+update(m, 10000) # I found it runs really quickly...
+
+# finally, get the samples from JAGS:
+draw <- coda.samples(m, 100000, thin=100, variable.names=c("x", "y"))
+
+# and get a pretty picture and summary stats:
+#plot(draw);
+#print(summary(draw))
+
+samples <- as.matrix(draw)
+

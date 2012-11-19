7fd6f4cc7c2d701a8162b15ccc251d41e36764eb
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-11-20 10:48:59 +1300

    Written up but uses tons of memory and doesn't run

--- JAGS/main.r
+++ JAGS/main.r
@@ -8,10 +8,10 @@ library(rjags)
 m = jags.model(file = "model.txt")#, data=data)
 
 # this will create a BUGS object in R. Now you can do some runs...
-update(m, 10000) # I found it runs really quickly...
+#update(m, 10000) # I found it runs really quickly...
 
 # finally, get the samples from JAGS:
-draw <- coda.samples(m, 100000, thin=100, variable.names=c("x", "y"))
+draw <- coda.samples(m, 10000, thin=1000, variable.names=c("alpha"))
 
 # and get a pretty picture and summary stats:
 #plot(draw);

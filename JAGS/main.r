208c190805f867b5865efdcff831b10d908122f2
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-11-20 10:58:18 +1300

    Runs but is HORIFICALLY SLOW even on sparse dataset

--- JAGS/main.r
+++ JAGS/main.r
@@ -1,21 +1,15 @@
 library(rjags)
 
-# create the data to be sent to R (using the same as class example):
-#data <- source('data.txt')$value
+# Load the "sparse" dataset
+data <- as.matrix(read.table('../c++_dnest/SimulatedData/sparse.txt', header=FALSE))
+data <- list(F=data)
 
-# now to send the model and data to JAGS:
-# if you need to navigate to the place you saved your .bug file, now is the time to do it.
-m = jags.model(file = "model.txt")#, data=data)
+# Create the JAGS model
+m <- jags.model(file = "model.txt", data=data)
 
-# this will create a BUGS object in R. Now you can do some runs...
-#update(m, 10000) # I found it runs really quickly...
+# Run JAGS (no burn-in)
+draw <- coda.samples(m, 10000, thin=10, variable.names=c("alpha"))
 
-# finally, get the samples from JAGS:
-draw <- coda.samples(m, 10000, thin=1000, variable.names=c("alpha"))
-
-# and get a pretty picture and summary stats:
-#plot(draw);
-#print(summary(draw))
-
+# Turn results into matrix
 samples <- as.matrix(draw)
 

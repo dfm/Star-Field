60a6b6a6dad2c21bf0c177677e59710eeb06d0bd
Brendon J. Brewer <brendon.brewer@gmail.com>
2012-11-30 10:41:38 +1300

    Can compute CDF (no changes to plotting)

--- paper/Figures/sex_N.py
+++ paper/Figures/sex_N.py
@@ -3,6 +3,24 @@ import numpy as np
 import matplotlib.pyplot as pl
 from matplotlib.ticker import MaxNLocator
 
+
+# CDF of p(flux|hyperparameters)
+def CDF(flux, params=[0.3, 0.6, 1.1, 2.]):
+	h1, h2, a1, a2 = params[0], params[1], params[2], params[3]
+	# Normalisation constants (from appendix)
+	Z1 = (h1**(-a1) - h2**(-a1))/a1 + h2**(-a1)/a2
+	Z2 = Z1*h2**(a1 - a2)
+	F = np.zeros(flux.shape)
+	subset = np.logical_and(flux > h1, flux <= h2)
+	F[subset] = (Z1*a1)**(-1)*(h1**(-a1) - flux[subset]**(-a1))
+	subset = flux > h2
+	F[subset] = 1. - (Z2*a2)**(-1)*flux[subset]**(-a2)
+	return F
+
+# Compute CDF
+flux = np.linspace(0., 20., 10001)
+F = CDF(flux)
+
 # Directories
 directories = ['break2', 'crowded']
 
@@ -29,11 +47,14 @@ for i in xrange(0, 2):
     pl.subplot(1, 2, i + 1)
     pl.plot(sigma, N, 'ko-', mfc="#888888", mew=1.5, lw=2)
     pl.axhline(82 * 10 ** i, color='k', linestyle='--', linewidth=2)
+#    pl.plot(13*flux, 82*10**i*(1. - F), color='r', linewidth=2)
     pl.xlabel(r'Detection Threshold ($\sigma$)')
     pl.ylabel('Number of Stars $N$')
+#    pl.xlim([0, 10])
     pl.ylim(0)
     pl.gca().xaxis.set_major_locator(MaxNLocator(4))
     pl.gca().yaxis.set_major_locator(MaxNLocator(4))
     pl.title('Test Case ' + str(i + 1))
 
 pl.savefig('sex_N.eps', bbox_inches='tight')
+

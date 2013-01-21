4fdbb419d41be8a4002a7e0c293c8a5beeed0c42
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-01-21 14:12:27 +1300

    Started working on particular star inferences

---
+++ paper/Figures/particular_stars.py
@@ -0,0 +1,8 @@
+# Investigate the inference about a particular star
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+ground_truth = \
+[np.loadtxt('../../c++_dnest/SimulatedData/100_ground_truth.txt'), \
+ np.loadtxt('../../c++_dnest/SimulatedData/1000_ground_truth.txt')]

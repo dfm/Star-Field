87ce39bb6c6a6b2c43a16788473918345af61693
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-02-25 15:03:33 +1300

    Show sextractor uncertainty

--- paper/Figures/particular_stars.py
+++ paper/Figures/particular_stars.py
@@ -28,7 +28,7 @@ fStars = fStars[good]
 
 # Pick a bright star
 ranks = np.argsort(fStars)
-which = np.nonzero(ranks == int(0.9*fStars.size))[0]
+which = np.nonzero(ranks == int(0.4*fStars.size))[0]
 
 # Make a little circular aperture around it
 xc = xStars[which]
@@ -80,6 +80,7 @@ sex_catalog = np.loadtxt('../../sextractor/100/stars.2.5.cat')
 xSex =  sex_catalog[:,5]*0.02 - 1. - 0.01
 ySex = -(sex_catalog[:,6]*0.02 - 1.) + 0.01
 fSex = sex_catalog[:,1]*0.0004/0.69666
+fErrSex = sex_catalog[:,2]*0.0004/0.69666
 plt.plot(xSex, ySex, 'bo', alpha=0.5)
 plt.axis('scaled')
 plt.show()
@@ -87,10 +88,13 @@ plt.show()
 rsq = (xSex - xc)**2 + (ySex - yc)**2
 within = rsq < radius**2
 sex_flux = fSex[within].sum()
+sex_fErr = np.sqrt((fErrSex[within]**2).sum())
 
-plt.hist(posterior_flux, 30, alpha=0.5, normed=True)
-plt.axvline(true_flux, color='k', linewidth=2, label='Inference')
-plt.axvline(sex_flux, color='r', linewidth=2, label='SExtractor')
+plt.hist(posterior_flux, 30, alpha=0.3, normed=True)
+plt.axvline(true_flux, color='k', linewidth=3, label='Inference')
+plt.axvline(sex_flux, color='r', linestyle='--', linewidth=3, label='SExtractor')
+plt.axvline(sex_flux - fErrSex[within], color='r', linestyle='--', linewidth=1)
+plt.axvline(sex_flux + fErrSex[within], color='r', linestyle='--', linewidth=1)
 plt.xlabel('Flux', fontsize=16)
 plt.ylabel('Posterior PDF', fontsize=16)
 plt.legend()
